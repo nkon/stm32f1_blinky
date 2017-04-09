f834d1a96f003863e24b2ac83dc0ee920a9ab875
KONDO Nobuhiro <kondou.nobuhiro@gmail.com>
2017-04-09 18:17:43 +0900

    Blink with SysTick and callback. Does not use asm.

--- src/main.rs
+++ src/main.rs
@@ -1,36 +1,37 @@
 #![no_std]
 #![no_main]
 #![feature(lang_items)]
-#![feature(asm)]
+#![allow(non_snake_case)]
 
 extern crate stm32cubef1;
 use stm32cubef1::*;
-use stm32f1xx_hal_gpio::{GPIOA, GPIO_PIN_5};
+use gpio;
+use gpio::{GPIOA, GPIO_PIN_5};
 
 #[no_mangle]
 pub extern fn rust_main() {
-    stm32f1xx_hal_gpio::GPIOA_CLK_ENABLE();
+    gpio::GPIOA_CLK_ENABLE();
 
-    let mut gpio_init_struct = stm32f1xx_hal_gpio::GPIO_InitTypeDef{Pin: 0, Mode: 0, Pull: 0, Speed: 0};
+    let mut gpio_init_struct = gpio::InitTypeDef{Pin: 0, Mode: 0, Pull: 0, Speed: 0};
     gpio_init_struct.Pin = 0x0020;
     gpio_init_struct.Mode = 0x0001;
     gpio_init_struct.Speed = 0x0002;
 
-    stm32f1xx_hal_gpio::Init(GPIOA(), &gpio_init_struct);
+    gpio::Init(GPIOA(), &gpio_init_struct);
+}
 
-    loop {
-        stm32f1xx_hal_gpio::WritePin(GPIOA(), GPIO_PIN_5, 1);
-        for _ in 1..400000 {
-            unsafe {
-                asm!("");
-            }
+static mut COUNT :u32 = 0;
+
+#[no_mangle]
+pub extern fn HAL_SYSTICK_Callback() {
+    unsafe {
+        COUNT = COUNT + 1;
+        if COUNT == 1000 {
+            gpio::WritePin(GPIOA(), GPIO_PIN_5, 1);
         }
-
-        stm32f1xx_hal_gpio::WritePin(GPIOA(), GPIO_PIN_5, 0);
-        for _ in 1..400000 {
-            unsafe {
-                asm!("");
-            }
+        if COUNT == 2000 {
+            gpio::WritePin(GPIOA(), GPIO_PIN_5, 0);
+            COUNT = 0;
         }
     }
 }
