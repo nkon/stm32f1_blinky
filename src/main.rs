d2c3c8eb95945940411f1452b67d111193467607
KONDO Nobuhiro <kondou.nobuhiro@gmail.com>
2017-04-09 23:40:46 +0900

    Follow stm32cubef1 update.

--- src/main.rs
+++ src/main.rs
@@ -6,16 +6,14 @@
 extern crate stm32cubef1;
 use stm32cubef1::*;
 use gpio;
-use gpio::{GPIOA, GPIO_PIN_5};
+use gpio::{GPIOA};
 
 #[no_mangle]
 pub extern fn rust_main() {
-    gpio::GPIOA_CLK_ENABLE();
-
     let mut gpio_init_struct = gpio::InitTypeDef{Pin: 0, Mode: 0, Pull: 0, Speed: 0};
-    gpio_init_struct.Pin = 0x0020;
-    gpio_init_struct.Mode = 0x0001;
-    gpio_init_struct.Speed = 0x0002;
+    gpio_init_struct.Pin = gpio::PIN_5 as u32;
+    gpio_init_struct.Mode = gpio::MODE_OUTPUT_PP;
+    gpio_init_struct.Speed = gpio::SPEED_FREQ_LOW;
 
     gpio::Init(GPIOA(), &gpio_init_struct);
 }
@@ -27,10 +25,10 @@ pub extern fn HAL_SYSTICK_Callback() {
     unsafe {
         COUNT = COUNT + 1;
         if COUNT == 1000 {
-            gpio::WritePin(GPIOA(), GPIO_PIN_5, 1);
+            gpio::WritePin(GPIOA(), gpio::PIN_5, 1);
         }
         if COUNT == 2000 {
-            gpio::WritePin(GPIOA(), GPIO_PIN_5, 0);
+            gpio::WritePin(GPIOA(), gpio::PIN_5, 0);
             COUNT = 0;
         }
     }
