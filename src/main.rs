9d1c86627e2ceb9cc980eb4c4236f7d3c6a4f021
KONDO Nobuhiro <kondou.nobuhiro@gmail.com>
2017-04-10 00:19:08 +0900

    Use new trait API of stm32cubef1.

--- src/main.rs
+++ src/main.rs
@@ -10,12 +10,11 @@ use gpio::{GPIOA};
 
 #[no_mangle]
 pub extern fn rust_main() {
-    let mut gpio_init_struct = gpio::InitTypeDef{Pin: 0, Mode: 0, Pull: 0, Speed: 0};
-    gpio_init_struct.Pin = gpio::PIN_5 as u32;
-    gpio_init_struct.Mode = gpio::MODE_OUTPUT_PP;
-    gpio_init_struct.Speed = gpio::SPEED_FREQ_LOW;
-
-    gpio::Init(GPIOA(), &gpio_init_struct);
+    GPIOA().Init(&gpio::InitTypeDef{
+            Pin: gpio::PIN_5 as u32,
+            Mode: gpio::MODE_OUTPUT_PP,
+            Pull: gpio::NOPULL,
+            Speed: gpio::SPEED_FREQ_LOW});
 }
 
 static mut COUNT :u32 = 0;
@@ -25,10 +24,10 @@ pub extern fn HAL_SYSTICK_Callback() {
     unsafe {
         COUNT = COUNT + 1;
         if COUNT == 1000 {
-            gpio::WritePin(GPIOA(), gpio::PIN_5, 1);
+            GPIOA().WritePin(gpio::PIN_5, 1);
         }
         if COUNT == 2000 {
-            gpio::WritePin(GPIOA(), gpio::PIN_5, 0);
+            GPIOA().WritePin(gpio::PIN_5, 0);
             COUNT = 0;
         }
     }
